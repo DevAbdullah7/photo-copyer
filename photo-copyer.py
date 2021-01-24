6ef77f14cede325c19a9b0f2a98ef3d3611e4824
Abdullah Alqahtani <67982210+xNothing@users.noreply.github.com>
2021-01-24 23:43:51 +0300

    Update photo-copyer.py

--- photo-copyer.py
+++ photo-copyer.py
@@ -1,8 +1,11 @@
-x = open(input('Enter Hardisk: ')+':\\\\Users\\\\'+input('Enter user name: ')+'\\\\'+input('whare is the File? ')+'\\\\'+input('Enter File name: ')+'\\\\'+input('Enter Photo name: '), 'rb')
-k = int(input('How much of copy do you need? '))
-l = input('Enter Hardisk: ')+':\\\\Users\\\\'+input('Enter user name: ')+'\\\\'+input('whare is the File? ')+'\\\\'+input('Enter File name: ')+'\\\\' * k
-p = input('Enter copy photo name: ')
+photo1 = open(input('Enter Hardisk: ')+':\\\\Users\\\\'+input('Enter user name: ')+'\\\\'+input('whare is the File? ')+'\\\\'+input('Enter File name: ')+'\\\\'+input('Enter Photo name: '), 'rb')
+count = int(input('How much of copy do you need? '))
+copy_file = input('Enter Hardisk: ')+':\\\\Users\\\\'+input('Enter user name: ')+'\\\\'+input('whare is the File? ')+'\\\\'+input('Enter File name: ')+'\\\\' * count
+photo2 = input('Enter copy photo name: ')
 
-for i in range(1, k+1):
-   i = open(l+p+str(i)+'.png', 'wb')
-   i.write(x.read())
+for i in range(1, count+1):
+   i = open(copy_file+photo2+str(i)+'.png', 'wb')
+   i.write(photo1.read())
+   i.close()
+   photo1.close()
+   
